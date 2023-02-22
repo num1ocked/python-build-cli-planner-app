29628511fd37f16c5e0570cad4c4dd4bbbf0bf97
num1ocked <105787413+num1ocked@users.noreply.github.com>
2023-02-22 22:47:48 +0000

    Update deadlined_reminders.py

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,7 +1,12 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta, abstractmethod, ABC
 from collections.abc import Iterable
 
 class DeadlineMetaReminder(Iterable, metaclass=ABCMeta):
+    @abstractmethod
+    def is_due(self):
+        pass
+
+class DeadlinedReminder(Iterable, ABC):
     @abstractmethod
     def is_due(self):
         pass
